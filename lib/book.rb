95b5ebeb752c4974390f4d8c9b16e5b27a173a03
Peter <peterkilungya1@gmail.com>
2022-10-31 11:30:02 +0300

    assignment completed

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,31 @@
+class Book
+    attr_accessor :author
+    attr_accessor :page_count
+    attr_accessor :genre
+    attr_accessor :turn_page
+    def initialize(title)
+        @title = title
+    end
+
+    def title
+        @title = "And Then There Were None"
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+
+def author
+    @author = "Agatha Christie"
+end
+
+def page_count
+    @page_count = "272"
+end
+
+def genre
+    @genre = "Mystery"
+end
 
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,40 @@
-# Make your shoe class here!
+class Shoe
+    attr_accessor :brand
+    attr_accessor :color
+    attr_accessor :size
+    attr_accessor :material
+    attr_accessor :condition
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def cobble
+        @condition = "new"
+        puts "Your shoe is as good as new!"
+
+    end
+end
+
+def brand
+    @brand = "Nike"
+end
+
+def color
+    @color = "red"
+end
+
+def size
+    @size = "9.5" 
+end
+
+def material
+    @material = "suede"
+end
+
+def condition
+    @condition = "tattered"
+end
+
+s1 = Shoe.new('Puma')
+
+#binding.pry
